7604aee2a4b7bb289e5e0c551312a39b285ec649
Sivaldo Oliveira <oliveira.smo@outlook.com>
2025-03-16 09:21:38 -0300

    Projeto CSharp

--- SimpleGameApi/SimpleGameApi/obj/Release/net6.0/SimpleGameApi.AssemblyInfo.cs
+++ SimpleGameApi/SimpleGameApi/obj/Release/net6.0/SimpleGameApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGameApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218eac27c7a0da8ec546fbf78aba39687d900ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGameApi")]
