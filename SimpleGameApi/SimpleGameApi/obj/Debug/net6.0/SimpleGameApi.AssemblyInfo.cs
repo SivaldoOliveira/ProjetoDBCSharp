c58670b7902de2d11c088e7f46c99e93984d8f52
Sivaldo Oliveira <oliveira.smo@outlook.com>
2025-03-17 21:05:43 -0300

    criação das entidades

--- SimpleGameApi/SimpleGameApi/obj/Debug/net6.0/SimpleGameApi.AssemblyInfo.cs
+++ SimpleGameApi/SimpleGameApi/obj/Debug/net6.0/SimpleGameApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4745c4ab9c584b6029e44d43d1f95c287698adc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c66affdc7ab5eea1537d09290dd9cfbf66a691")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
