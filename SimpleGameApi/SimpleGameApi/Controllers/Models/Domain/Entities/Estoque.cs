4de6ac87f6fcde3add917d7c6c1397285979ed49
Sivaldo Oliveira <oliveira.smo@outlook.com>
2025-03-27 21:47:48 -0300

    Implementação da repo de jogos

--- SimpleGameApi/SimpleGameApi/Controllers/Models/Domain/Entities/Estoque.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SimpleGameApi.Controllers.Models.Domain.Entities;
-
-public class Estoque
-{
-    public int Id { get; set; }
-    public int IdJogo { get; set; }
-    public int Quantidade { get; set; }
-}
---
+++ SimpleGameApi/SimpleGameApi/Models/Domain/Contracts/Repositories/IAluguelRepository.cs
@@ -0,0 +1,10 @@
+﻿using SimpleGameApi.Models.Domain.Contracts.Base;
+using SimpleGameApi.Models.Domain.Entities;
+
+namespace SimpleGameApi.Models.Domain.Contracts.Repositories
+{
+    public interface IAluguelRepository : IGenericRepository<Aluguel, int>
+    {
+
+    }
+}
