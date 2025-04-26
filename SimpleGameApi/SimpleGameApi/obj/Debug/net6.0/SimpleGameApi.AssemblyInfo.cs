3ef0c77ca1a221f467052105914114bebe13fd95
SivaldoOliveira <oliveira.smo@outlook.com>
2025-04-26 12:42:24 -0300

    Aula-24

--- SimpleGameApi/SimpleGameApi/obj/Debug/net6.0/SimpleGameApi.AssemblyInfo.cs
+++ SimpleGameApi/SimpleGameApi/obj/Debug/net6.0/SimpleGameApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b15955d11247a1c158671a1081fefd3fffca230")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f63c918e6e6a6d59f9f4028bab3363e607501fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
